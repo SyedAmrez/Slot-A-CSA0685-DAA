d8ad0eb3d170fcf7d3f08e5de91eef58822178ba
SyedAmrez <112709867+SyedAmrez@users.noreply.github.com>
2023-08-05 14:49:20 +0530

    OUTPUT

--- 5.Factorial.c
+++ 5.Factorial.c
@@ -4,9 +4,9 @@ int main()
  	int i,fact=1,number;    
  	printf("Enter a number: ");    
   	scanf("%d",&number);    
-    for(i=1;i<=number;i++)
+    	for(i=1;i<=number;i++)
 	{    
-    	fact=fact*i;    
+    		fact=fact*i;    
   	}    
   	printf("Factorial of %d is: %d",number,fact);    
 	return 0;  
