e6761ad79d66b55c215c9371efb2fbf21a85c17d
SyedAmrez <112709867+SyedAmrez@users.noreply.github.com>
2023-08-05 14:53:04 +0530

    OUTPUT

--- 9.Multiplication of matrix.c
+++ 9.Multiplication of matrix.c
@@ -1,9 +1,8 @@
 #include<stdio.h>    
-#include<stdlib.h>  
+#include<stdlib.h> 
 int main()
 {  
-	int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
-	system("cls");  
+	int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;      
 	printf("enter the number of row=");    
 	scanf("%d",&r);    
 	printf("enter the number of column=");    
